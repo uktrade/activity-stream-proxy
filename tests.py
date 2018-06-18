b48ddeb99b486104711e8110f11d54dbb60b4563
Michal Charemza <michal@charemza.name>
2018-06-18 14:44:06 +0100

    (feat) Use consistently denoramlised environment
    This does share code with the activity-stream. However, we do not factor it
    out, since we don't actually expect to use this repo. It's only here for
    just-in-case we do want a stand alone proxy in the future, so we can pen test
    it now and be to quickly use it it later if we need to. Slightly un-agile, but
    the cost of pen testing later is high, both in terms of money and time.

--- tests.py
+++ tests.py
@@ -383,10 +383,11 @@ async def post_text_no_content_type(url, auth, x_forwarded_for):
 
 def mock_env():
     return {
-        'INCOMING_ACCESS_KEY_PAIRS': (
-            'incoming-some-id-1:incoming-some-secret-1,' +
-            'incoming-some-id-2:incoming-some-secret-2'
-        ),
-        'INCOMING_IP_WHITELIST': '1.2.3.4,2.3.4.5',
+        'INCOMING_ACCESS_KEY_PAIRS__1__KEY_ID': 'incoming-some-id-1',
+        'INCOMING_ACCESS_KEY_PAIRS__1__SECRET_KEY': 'incoming-some-secret-1',
+        'INCOMING_ACCESS_KEY_PAIRS__2__KEY_ID': 'incoming-some-id-2',
+        'INCOMING_ACCESS_KEY_PAIRS__2__SECRET_KEY': 'incoming-some-secret-2',
+        'INCOMING_IP_WHITELIST__1': '1.2.3.4',
+        'INCOMING_IP_WHITELIST__2': '2.3.4.5',
         'PORT': '8080',
     }
